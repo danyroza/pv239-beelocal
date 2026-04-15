8fd17a1ca673e17adfc7219ec1b617c4bd3da09b
Natalia Ligacova <x539936@fi.muni.cz>
2026-04-15 02:01:18 +0200

    fix: use hasRoute<T>() for destination matching

--- app/src/main/java/com/pv239/beelocal/navigation/AppNavGraph.kt
+++ app/src/main/java/com/pv239/beelocal/navigation/AppNavGraph.kt
@@ -1,8 +1,11 @@
 package com.pv239.beelocal.navigation
 
+import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -10,6 +13,7 @@ import com.pv239.beelocal.BeelocalApp
 import com.pv239.beelocal.permissions.LocationPermissionScreen
 import com.pv239.beelocal.permissions.PermissionViewModel
 
+@SuppressLint("RestrictedApi")
 @Composable
 fun AppNavGraph(permissionViewModel: PermissionViewModel = hiltViewModel()) {
 
@@ -31,10 +35,9 @@ fun AppNavGraph(permissionViewModel: PermissionViewModel = hiltViewModel()) {
 
     LaunchedEffect(hasLocationPermission) {
         val currentRoute = navController.currentBackStackEntry?.destination
-        if (hasLocationPermission && currentRoute != MainGraph) {
+        if (hasLocationPermission && currentRoute?.hierarchy?.any { it.hasRoute<MainGraph>() } != true) {
             navController.navigate(MainGraph) {
                 popUpTo<PermissionsRoute> { inclusive = true }
-                launchSingleTop = true
             }
         }
     }

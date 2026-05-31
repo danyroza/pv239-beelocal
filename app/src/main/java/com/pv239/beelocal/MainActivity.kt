deffefec21eabeee123b5b511283fd6901c932ba
Jakub Wolek <kubkowolek@gmail.com>
2026-05-31 20:28:43 +0200

    fix: use correct user agent

--- app/src/main/java/com/pv239/beelocal/MainActivity.kt
+++ app/src/main/java/com/pv239/beelocal/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.google.android.datatransport.BuildConfig
 import com.pv239.beelocal.navigation.AppNavGraph
 import com.pv239.beelocal.ui.theme.BeelocalTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,7 +15,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Configuration.getInstance().userAgentValue = "BeeLocal"
+        Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
         enableEdgeToEdge()
         setContent {
             BeelocalTheme {
